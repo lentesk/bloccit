32787580e370262303e04cb70f7c14501f450d12
lentesk <lentesk@gmail.com>
2018-09-02 19:02:30 -0500

    testing assignment completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,4 +14,11 @@ require 'rails_helper'
        expect(response).to render_template("about")
      end
    end
+
+   describe "GET faq" do
+     it "renders the faq template" do
+       get :faq
+       expect(response).to render_template("faq")
+     end
+   end
  end
