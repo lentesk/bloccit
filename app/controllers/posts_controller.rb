02ef4d7cd462aeebb4b9c430af6c729a14242e54
lentesk <lentesk@gmail.com>
2018-09-07 15:55:08 -0500

    stuck at GET show test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def new
@@ -14,5 +15,16 @@ class PostsController < ApplicationController
   end
 
   def create
+		@post = Post.new
+		@post.title = params[:post][:title]
+		@post.body = params[:post][:body]
+
+		if @post.save
+			flash[:notice] = "Post was saved."
+			redirect_to @post
+		else
+			flash.now[:alert] = "There was an error saving the post. Please try again."
+			render :new
+  	end
   end
 end
