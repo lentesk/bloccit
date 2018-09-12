349d21320ca36bae9d8a4fd4891842c4e1f3012b
lentesk <lentesk@gmail.com>
2018-09-12 18:58:50 -0500

    topics and posts checkpoint completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
  require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
- # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph)
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
    )
  end
+ topics = Topic.all
+
+
+ # Create Posts
+ 50.times do
+   Post.create!(
+     topic:  topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+
  posts = Post.all
  
  # Create Comments
- # #3
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
  
+
  p "#{Post.count}"
  Post.find_or_create_by(title: "assignment post", body: "assignment body")
  p "#{Post.count}"
  
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
